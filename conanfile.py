04d20685defb05bbb8f1e7ea8991729f03d0085c
Croydon <cr0ydon@outlook.com>
2018-02-18 07:33:49 +0100

    Update to v3.1

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,15 @@
-from conans import ConanFile, tools
-import os
+from conans import ConanFile
 
 
 class KainjowmustacheConan(ConanFile):
     name = "Kainjow_Mustache"
-    version = "2.0"
+    version = "3.1"
     license = "Boost Software License - Version 1.0"
     # No settings/options are necessary, this is header only
     build_policy = "missing" # header only no need to build it
 
     def source(self):
-        self.run("git clone --branch v2.0 --depth 1 https://github.com/kainjow/Mustache")
+        self.run("git clone --branch v%s --depth 1 https://github.com/kainjow/Mustache" % (self.version))
 
     def package(self):
         self.copy("mustache.hpp", dst="include/kainjow", src="Mustache")
