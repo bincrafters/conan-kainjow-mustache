98f0c3ecb838a521ce6d7250b4bbc87640dd2fd0
Croydon <cr0ydon@outlook.com>
2018-07-25 05:12:12 +0200

    Modern recipe

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile
+from conans import ConanFile, tools
+import os
 
 
 class KainjowmustacheConan(ConanFile):
@@ -6,15 +7,20 @@ class KainjowmustacheConan(ConanFile):
     version = "3.1"
     license = "Boost Software License - Version 1.0"
     url = "https://github.com/inexorgame/conan-kainjow-mustache"
+    homepage = "https://github.com/kainjow/Mustache"
     description = "Mustache text templates for modern C++"
     # No settings/options are necessary, this is header only
-    build_policy = "missing" # header only no need to build it
+    no_copy_source = True
+
+    source_subfolder = "Mustache"
 
     def source(self):
-        self.run("git clone --branch v%s --depth 1 https://github.com/kainjow/Mustache" % (self.version))
+        archive_url = "https://github.com/kainjow/Mustache/archive/v{!s}.zip".format(self.version)
+        tools.get(archive_url, sha256="f327d3b9c7313f7efcb1209f44fb10a5e65da27d28bbdc4e0518443c3add0dc5")
+        os.rename("Mustache-{!s}".format(self.version), self.source_subfolder)
 
     def package(self):
-        self.copy("mustache.hpp", dst="include/kainjow", src="Mustache")
+        self.copy("mustache.hpp", dst="include/kainjow", src=self.source_subfolder)
 
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']  # Ordered list of include paths
+    def package_id(self):
+        self.info.header_only()
